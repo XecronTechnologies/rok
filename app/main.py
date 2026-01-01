2470be26f33b036a331fa6a98dde75990913bea4
csathishkumar <csathishkumar2002@gmail.com>
2026-01-01 16:57:41 +0530

    firebase code commented

--- app/main.py
+++ app/main.py
@@ -14,13 +14,13 @@ app = FastAPI()
 async def root():
     return {"message": "FastAPI + Neon ready"}
 
-@app.get("/users")
-async def list_users():
-    try:
-        users = await fetch_users()
-        return users
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+# @app.get("/users")
+# async def list_users():
+#     try:
+#         users = await fetch_users()
+#         return users
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
 
 @app.post("/rok_db")
 async def fn_rok_db(input_map: dict):
@@ -30,47 +30,47 @@ async def fn_rok_db(input_map: dict):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-@app.get("/get_access_token")
-async def get_access_token(input_map: Optional[dict]):
-    try:
-        record = await fn_route(input_map)
-        return record
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+# @app.get("/get_access_token")
+# async def get_access_token(input_map: Optional[dict]):
+#     try:
+#         record = await fn_route(input_map)
+#         return record
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
 
-@app.post("/users")
-async def add_user(user_data: Dict[str, Any]):
-    try:
-        # Basic validation
-        if not user_data.get("name") or not user_data.get("email"):
-            raise HTTPException(status_code=400, detail="Name and email are required")
+# @app.post("/users")
+# async def add_user(user_data: Dict[str, Any]):
+#     try:
+#         # Basic validation
+#         if not user_data.get("name") or not user_data.get("email"):
+#             raise HTTPException(status_code=400, detail="Name and email are required")
         
-        name = user_data["name"]
-        email = user_data["email"]
+#         name = user_data["name"]
+#         email = user_data["email"]
         
-        user = await fn_add_record({"name":name,"email":email})
-        return user
-    except asyncpg.exceptions.UniqueViolationError:
-        raise HTTPException(status_code=400, detail="Email already exists")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+#         user = await fn_add_record({"name":name,"email":email})
+#         return user
+#     except asyncpg.exceptions.UniqueViolationError:
+#         raise HTTPException(status_code=400, detail="Email already exists")
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
 
 
 
-@app.get("/firebase/{collection_name}/{document_id}")
-async def get_document(collection_name: str, document_id: str):
-    cred = credentials.Certificate(os.path.join(os.path.dirname(__file__), "service_account.json"))
-    firebase_admin.initialize_app(cred)
-    db = firestore.client()
-    if not db:
-        raise HTTPException(status_code=503, detail="Firebase not configured or initialized")
-    try:
-        doc_ref = db.collection(collection_name).document(document_id)
-        doc = doc_ref.get()
-        temp =  json.loads(json.dumps({"as":doc.to_dict()}))
-        temp["id"] = doc.to_dict().get("id")
-        return temp
+# @app.get("/firebase/{collection_name}/{document_id}")
+# async def get_document(collection_name: str, document_id: str):
+#     cred = credentials.Certificate(os.path.join(os.path.dirname(__file__), "service_account.json"))
+#     firebase_admin.initialize_app(cred)
+#     db = firestore.client()
+#     if not db:
+#         raise HTTPException(status_code=503, detail="Firebase not configured or initialized")
+#     try:
+#         doc_ref = db.collection(collection_name).document(document_id)
+#         doc = doc_ref.get()
+#         temp =  json.loads(json.dumps({"as":doc.to_dict()}))
+#         temp["id"] = doc.to_dict().get("id")
+#         return temp
 
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-6
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
+# 6
